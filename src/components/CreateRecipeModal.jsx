2902edee5664a7d2e59691e76ee61d38ba383a28
KWangechi <rachel.karanja@strathmore.edu>
2024-12-14 23:34:09 +0300

    feat: added form validations to the recipe form

--- src/components/CreateRecipeModal.jsx
+++ src/components/CreateRecipeModal.jsx
@@ -17,6 +17,12 @@ export default function CreateRecipeModal({
   setViewRecipe,
 }) {
   const [photoUrl, setPhotoUrl] = useState(null);
+  const [errors, setErrors] = useState({
+    name: "",
+    category: "",
+    ingredients: [],
+    instructions: [],
+  });
 
   const editMode = !!selectedRecipe;
   //   const [open, setOpen] = useState(true);
@@ -47,6 +53,10 @@ export default function CreateRecipeModal({
   };
 
   const handleAddRecipe = () => {
+    const isValid = validateInputs();
+    if (!isValid) {
+      return;
+    }
     if (editMode) {
       updateRecipe(selectedRecipe?.id, recipe);
     } else {
@@ -61,6 +71,23 @@ export default function CreateRecipeModal({
       setViewRecipe(false);
     }
   };
+
+  // populate the errors state with the appropriate messages
+  function validateInputs() {
+    const { name, category, ingredients, instructions } = recipe;
+
+    const newErrors = {
+      name: name.length === 0 ? "Recipe name is required" : "",
+      category: category === "" ? "Category is required" : "",
+      ingredients: ingredients.length === 0 ? "Ingredients are required" : "",
+      instructions:
+        instructions.length === 0 ? "Instructions are required" : "",
+    };
+
+    setErrors(newErrors);
+
+    return Object.values(newErrors).every((error) => error === "");
+  }
 
   return (
     <Dialog open={open} onClose={setOpen} className="relative z-10">
@@ -84,9 +111,23 @@ export default function CreateRecipeModal({
                 <div className="sm:col-span-6">
                   <label
                     htmlFor="recipename"
-                    className="block text-sm/6 font-medium text-gray-900"
+                    className="flex text-sm/6 font-medium text-gray-900"
                   >
                     Recipe Name
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="red"
+                      className="size-3"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                      />
+                    </svg>
                   </label>
                   <div className="mt-2">
                     <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
@@ -103,6 +144,14 @@ export default function CreateRecipeModal({
                         }
                       />
                     </div>
+
+                    <div className="mt-2">
+                      {errors.name && (
+                        <div className="text-red-500 text-xs">
+                          {errors.name}
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </div>
 
@@ -110,10 +159,25 @@ export default function CreateRecipeModal({
                 <div className="sm:col-span-6">
                   <label
                     htmlFor="category"
-                    className="block text-sm/6 font-medium text-gray-900"
+                    className="flex text-sm/6 font-medium text-gray-900"
                   >
                     Category
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="red"
+                      className="size-3"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                      />
+                    </svg>
                   </label>
+
                   <div className="mt-2 grid grid-cols-1">
                     <select
                       id="category"
@@ -137,6 +201,14 @@ export default function CreateRecipeModal({
                       aria-hidden="true"
                       className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
                     />
+                  </div>
+
+                  <div className="mt-2">
+                    {errors.category && (
+                      <div className="text-red-500 text-xs">
+                        {errors.category}
+                      </div>
+                    )}
                   </div>
                 </div>
 
@@ -169,9 +241,23 @@ export default function CreateRecipeModal({
                 <div className="col-span-full">
                   <label
                     htmlFor="instructions"
-                    className="block text-sm/6 font-medium text-gray-900"
+                    className="flex text-sm/6 font-medium text-gray-900"
                   >
                     Instructions
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="red"
+                      className="size-3"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                      />
+                    </svg>
                   </label>
                   <div className="mt-2">
                     <textarea
@@ -189,15 +275,37 @@ export default function CreateRecipeModal({
                       required
                     />
                   </div>
+
+                  <div className="mt-2">
+                    {errors.instructions && (
+                      <div className="text-red-500 text-xs">
+                        {errors.instructions}
+                      </div>
+                    )}
+                  </div>
                 </div>
 
                 {/* Ingredients */}
                 <div className="col-span-full">
                   <label
                     htmlFor="ingredients"
-                    className="block text-sm/6 font-medium text-gray-900"
+                    className="flex text-sm/6 font-medium text-gray-900"
                   >
                     Ingredients
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="red"
+                      className="size-3"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                      />
+                    </svg>
                   </label>
                   <div className="mt-2">
                     <textarea
@@ -214,6 +322,14 @@ export default function CreateRecipeModal({
                         });
                       }}
                     />
+                  </div>
+
+                  <div className="mt-2">
+                    {errors.ingredients && (
+                      <div className="text-red-500 text-xs">
+                        {errors.ingredients}
+                      </div>
+                    )}
                   </div>
                 </div>
 
